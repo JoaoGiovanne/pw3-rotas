63e499866056e91918e085c0af401efd3eea00ce
Etec Prof. Horácio Augusto da Silveira <joaogiovanneetec@gmail.com>
2024-05-28 11:03:11 -0300

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'sobre/:id', component: SobreComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NaoEncontradoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sobre.component.css']
 })
 export class SobreComponent {
+  id: number = 0;
 
+  constructor(private route: ActivatedRoute){
+    this.route.params.subscribe(res => this.id = res['id']);
+  }
 }
